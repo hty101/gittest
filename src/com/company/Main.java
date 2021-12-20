2201adcd08a73806043e9d595ce1380d3c6e65c7
layner@163.com <Layne@atguigu.com>
2021-12-20 23:06:55 +0800

    the push first gittest

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,5 +9,11 @@ public class Main {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("pushgit");
     }
 }
