defb4bcf8956a9853796395539086fa1e907291e
layner@163.com <Layne@atguigu.com>
2021-12-20 22:50:13 +0800

    the java first gittest

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,8 @@ public class Main {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
